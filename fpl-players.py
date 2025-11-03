c7db8ca3feeba0c484f12c7f71704f5d373ee482
Fergus Walden <spoicycurri33@gmail.com>
2025-11-03 12:51:48 +0000

    create fpl dataset

--- fpl-players.py
+++ fpl-players.py
@@ -37,6 +37,7 @@ PLAYER_NAME_MANUAL = {
     'Norberto Bercique Gomes Betuncal': 'Beto',
     'João Pedro Ferreira da Silva': 'Jota Silva'
 }
+UPDATE_REF=False
 
 
 def get_fbref():
@@ -64,27 +65,35 @@ def get_fpl_data():
     return df
 
 
-def print_comparison_metrics(df, fuzzy_df):
+def get_ref_data():
+    try:
+        return pd.read_csv('reference_player_names.csv')
+    except FileNotFoundError:
+        return pd.DataFrame(columns=['player_code', 'fbref_name', 'fpl_name'])
+
+
+def print_comparison_metrics(df):
     rows = df.shape[0]
     exact_matches = df['Player'].notnull().sum()
-    fuzzy_matches = fuzzy_df['Manual Override'].isnull().sum()
-    manual_matches = fuzzy_df['Manual Override'].notnull().sum()
+    fuzzy_matches = df['Manual Override'].isnull().sum() - exact_matches
+    manual_matches = df['Manual Override'].notnull().sum()
+    missing_matches = df[df['name_match'].isnull()]['fpl_name'].tolist()
     
     print(f"Total players: {rows}")
     print(f"Exact matches: {exact_matches}; {exact_matches/rows:.1%}")
     print(f"Fuzzy matches: {fuzzy_matches}; {fuzzy_matches/rows:.1%}")
     print(f"Manual matches: {manual_matches}; {manual_matches/rows:.1%}")
+    print(f"missing matches: {len(missing_matches)}; {len(missing_matches)/rows:.1%}; {missing_matches}")
     return None
 
 
-def suggest_fuzzy_matches(df_fpl, df_fbref, threshold=80):
+def suggest_fuzzy_matches(fpl_names, fbref_names, threshold=80):
     """
     Perform fuzzy matching between FPL and FBRef player names
     """
-    fbref_names = df_fbref['Player'].tolist()
-    
+
     matches = []
-    for _, fpl_name in enumerate(df_fpl['fullname']):
+    for _, fpl_name in enumerate(fpl_names):
         # Get best match
         best_match = process.extractOne(
             fpl_name, 
@@ -103,34 +112,82 @@ def suggest_fuzzy_matches(df_fpl, df_fbref, threshold=80):
                 'fpl_name': fpl_name,
                 'fbref_name': None,
                 'score': 0
-            })
-            
-    best_matches_df = pd.DataFrame(matches)
-    best_matches_df['Manual Override'] = best_matches_df['fpl_name'].map(PLAYER_NAME_MANUAL)
-    best_matches_df = best_matches_df.sort_values(by='score', ascending=False)
-    best_matches_df.to_csv("fuzzy_matches_debug.csv", index=False)
-    return best_matches_df
+            })      
+    return pd.DataFrame(matches)
 
 
-def main():
-    df_fbref = get_fbref()
-    df_fpl = get_fpl_data()
-    df = pd.merge(df_fbref, df_fpl, left_on='Player', right_on='fullname', how='right')
-    missing_players = df[df['Player'].isnull()]
-    best_matches_df = suggest_fuzzy_matches(missing_players, df_fbref, threshold=30)
-    print_comparison_metrics(df, best_matches_df)
+def update_reference_names(exact_matches):
+    # Fuzzy Matches
+    fuzzy_matches = pd.read_csv("fuzzy_matches_debug.csv")
     
-    df = pd.merge(
-        df,
-        best_matches_df[['fpl_name', 'fbref_name', 'Manual Override']],
+    # Combine new matches
+    new_matches = pd.merge(
+        exact_matches,
+        fuzzy_matches[['fpl_name', 'fbref_name', 'Manual Override']],
         left_on='fullname',
         right_on='fpl_name',
         how='left'
     )
-    df['name_match'] = df['Player'].combine_first(df['Manual Override']).combine_first(df['fbref_name'])
+    new_matches['name_match'] = new_matches['Player'].combine_first(new_matches['Manual Override']).combine_first(new_matches['fbref_name'])
+    
+    print_comparison_metrics(new_matches)
 
-    df = df[['name_match'] + list(COLUMNS.values())]
+    new_matches = new_matches[['player_code', 'name_match', 'fullname']]
+    new_matches = new_matches.rename(columns={
+        'name_match': 'fbref_name',
+        'fullname': 'fpl_name'
+    })
 
+    # Update reference file
+    ref_names = get_ref_data()
+    ref_names_new = pd.concat([ref_names, new_matches], ignore_index=True)
+    ref_names_new.to_csv("reference_player_names.csv", index=False)
+    
+    return None
+
+
+def match_player_names(df_fpl):
+    df_fbref = get_fbref()
+    df_fpl_missing = df_fpl.loc[df_fpl['fbref_name'].isnull(), ['player_code', 'fullname']]
+    
+    # Exact Matches
+    df = pd.merge(df_fbref, df_fpl_missing, left_on='Player', right_on='fullname', how='right')
+    
+    # Fuzzy Matches
+    no_matches = df[df['Player'].isnull()]['fullname']
+    fbref_names = df_fbref['Player'].tolist()
+    fuzzy_matches = suggest_fuzzy_matches(no_matches, fbref_names, threshold=30)
+    
+    # Manual Override
+    fuzzy_matches['Manual Override'] = fuzzy_matches['fpl_name'].map(PLAYER_NAME_MANUAL)
+    
+    print("review fuzzy matches and update manual overrides as needed...")
+    fuzzy_matches = fuzzy_matches.sort_values(by='score', ascending=False)
+    fuzzy_matches.to_csv("fuzzy_matches_debug.csv", index=False)
+
+    return df
+
+
+def main():
+    # Load data
+    df_fpl = get_fpl_data()
+    ref_names = get_ref_data()
+    
+    # Match names
+    df = pd.merge(df_fpl, ref_names, on='player_code', how='left')
+
+    if df['fbref_name'].isnull().sum() > 0:
+        print("Some player names are missing from the reference dataset. Update reference data...")
+        df = match_player_names(df)
+        
+        if UPDATE_REF:
+            update_reference_names(df)
+            raise ValueError("Reference Data updated. Please re-run the script.")
+
+        raise ValueError("Review fuzzy_matches_debug.csv for potential matches. Update Manual Overrides as needed, and re-run with UPDATE_REF=True to update reference data.")
+    
+    df = df[['player_code', 'fbref_name', 'position', 'fpl_cost', 'fpl_form', 'season_ppg', 'total_points']]
+    
     return df
 
 
